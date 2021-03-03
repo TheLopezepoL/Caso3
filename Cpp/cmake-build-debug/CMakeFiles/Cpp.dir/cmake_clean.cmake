acdd0c6d0ff5a51c413656fcaa07bbdfaead1c6e
Sebastián <thelopezepol@outlook.es>
2021-03-03 11:15:23 -0600

    Project in cpp

---
+++ Cpp/cmake-build-debug/CMakeFiles/Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp.dir/main.cpp.obj"
+  "Cpp.exe"
+  "Cpp.exe.manifest"
+  "Cpp.lib"
+  "Cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
