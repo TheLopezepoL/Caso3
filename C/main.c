5c59b2e8ec8a346310ae3273a8fa808796e1286f
Daniel Bejarano <dbejarano820@gmail.com>
2021-03-03 11:34:02 -0600

    C changes

--- C/main.c
+++ C/main.c
@@ -1,5 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int array[2000000];
+
+ int linearSearch(int arr[], int size, int num){
+     for(int i = 0; i<size; i++){
+         if(arr[i] == num)
+            return arr[i];
+     }
+     return -1;
+ }
+
  int main(){
 
+     int size = sizeof(array) / sizeof(array[0]);
+
+     for(int i = 0; i < size; i++){
+         array[i] = rand() % 10;
+     }
+
+     linearSearch(array, size, 15);
+    
 
      return 0;
  }
