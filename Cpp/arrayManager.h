ec94b9af8c12d334cc6a65f8f8d014d20b9616fa
Sebastián <thelopezepol@outlook.es>
2021-03-03 12:11:51 -0600

    Cpp terminado

---
+++ Cpp/arrayManager.h
@@ -0,0 +1,30 @@
+//
+// Created by thelo on 003 3 mar. 2021.
+//
+
+#ifndef CPP_ARRAYMANAGER_H
+#define CPP_ARRAYMANAGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+void fillArray(int array[], int size) {
+    srand(time(nullptr));
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % 10;
+    }
+}
+
+bool isInArray(int array[], int size, int search = 11){
+    for (int i=0; i < size; i++){
+        if (array[i] == search)
+            return true;
+    }
+    return false;
+}
+
+#endif //CPP_ARRAYMANAGER_H
--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include "arrayManager.h"
+#include <chrono>
+
+using namespace std::chrono;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Creacion del Array
+    static int array[10000000];
+    int size = sizeof(array) / sizeof(*array);
+    fillArray(array, size);
+
+    //Analizar tiempo y memoria usados buscando un int
+    auto start = high_resolution_clock::now();
+    bool found = isInArray(array, size);
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    cout << "El proceso se ejecuto en: " << duration.count() << " milisegundos." << endl;
+
+    //Resultado de la busqueda
     return 0;
 }
