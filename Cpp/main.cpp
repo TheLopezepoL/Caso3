cadfcff52c71bf054e95c4fa3ba4e5bec3152d0d
Sebastián <thelopezepol@outlook.es>
2021-03-03 23:52:14 -0600

    Help con la ram

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,5 +1,7 @@
 #include "arrayManager.h"
 #include <chrono>
+#include "windows.h"
+#include "psapi.h"
 
 using namespace std::chrono;
 
@@ -11,14 +13,31 @@ int main() {
     fillArray(array, size);
 
     //Analizar tiempo y memoria usados buscando un int
-    clock_t time;
-    time = clock();
+    PROCESS_MEMORY_COUNTERS_EX pmc;
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+    SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
+    cout << physMemUsedByMe << endl;
+
+    clock_t time = clock();
     bool found = isInArray(array, size);
     time = clock() - time;
 
-     double time_taken = double(time) / double(CLOCKS_PER_SEC); 
+    PROCESS_MEMORY_COUNTERS_EX pmcTwo;
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmcTwo, sizeof(pmcTwo));
+    SIZE_T physMemUsedByMeTwo = pmc.WorkingSetSize;
+    cout << physMemUsedByMeTwo << endl;
 
-     printf("linearSearch() took %f seconds to execute \n", time_taken); 
+     double time_taken = double(time) / double(CLOCKS_PER_SEC);
+     auto memory_taken = physMemUsedByMeTwo - physMemUsedByMeTwo;
+
+     printf("linearSearch() took %f seconds to execute. \n", time_taken);
+     printf("linearSearch() took %lu bits to execute. \n", memory_taken);
+
+     //Resultado del proceso
+     if (found)
+         printf("Se encontro el numero.\n");
+     else
+         printf("No se encontro el numero.\n");
 
     return 0;
 }
