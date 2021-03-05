30bef3013bc97a82ae1a1480da709aaf28e779af
Sebastián <thelopezepol@outlook.es>
2021-03-04 18:44:51 -0600

    Terminado

--- C/main.c
+++ C/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h> 
-
- int array[10000000];
+#include <stdbool.h>
+#include "windows.h"
+#include "psapi.h"
+//10420072
+//8773632
+ int array[20000000];
 
  bool linearSearch(int arr[], int size, int num){
      for(int i = 0; i<size; i++){
@@ -26,8 +29,15 @@
      time = clock();
      linearSearch(array, size, 15);
      time = clock() - time;
+
+     PROCESS_MEMORY_COUNTERS_EX pmc;
+     GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+     SIZE_T memory_taken = pmc.WorkingSetSize;
+
      double time_taken = ((double)time)/CLOCKS_PER_SEC;
 
-     printf("linearSearch() took %f seconds to execute \n", time_taken); 
+     printf("linearSearch() took %f seconds to execute. \n", time_taken);
+     printf("linearSearch() took %lu second to execute. \n", memory_taken);
+
      return 0;
  }
--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -8,7 +8,7 @@ using namespace std::chrono;
 
 int main() {
     // Creacion del Array
-    static int array[2000000];
+    static int array[20000000];
     int size = sizeof(array) / sizeof(*array);
     fillArray(array, size);
 
@@ -23,8 +23,9 @@ int main() {
 
      double time_taken = double(time) / double(CLOCKS_PER_SEC);
 
-     printf("linearSearch() took %f seconds to execute. \n", time_taken);
-     printf("linearSearch() took %lu bits to execute. \n", memory_taken);
+     printf("isInArray() took %f seconds to execute. \n", time_taken);
+     printf("isInArray() took %lu bits to execute. \n", memory_taken);
+
 
     return 0;
 }
